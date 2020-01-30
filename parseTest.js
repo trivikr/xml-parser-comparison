3595e8325633f0943e47531c7223df7ee4c4db79
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2020-01-29 16:24:48 -0800

    chore: prettify output before printing

--- parseTest.js
+++ parseTest.js
@@ -1,11 +1,15 @@
 const pixlXml = require("pixl-xml");
 const fastXmlParser = require("fast-xml-parser");
+const prettier = require("prettier");
+
 const { readFileSync } = require("fs");
 
 const xmlData = readFileSync("example.xml").toString();
+const printObject = data =>
+  prettier.format(JSON.stringify(data), { parser: "json" });
 
 console.log("Parsed by pixl-xml:");
-console.log(pixlXml.parse(xmlData));
+console.log(printObject(pixlXml.parse(xmlData)));
 
 console.log("\nParsed by fast-xml-parser:");
-console.log(fastXmlParser.parse(xmlData));
+console.log(printObject(fastXmlParser.parse(xmlData)));
